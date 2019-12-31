5c5dacea0dad72584d8edf4514642c71056a4e26
dylan <dylanalvarez1995@gmail.com>
2019-12-31 14:45:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+module.exports = {
+  "extends": "tslint:recommended",
+  "rules": {
+    "max-line-length": {
+      "options": [
+        100
+      ]
+    },
+    "only-arrow-functions": true,
+    "arrow-parens": [
+      true,
+      "ban-single-arg-parens"
+    ],
+    "semicolon": [
+      true,
+      "always"
+    ],
+    "indent": [
+      true,
+      "spaces",
+      2
+    ],
+    "forin": true,
+    "curly": [
+      true,
+      "ignore-same-line"
+    ],
+    "comment-format": [
+      true,
+      "check-space"
+    ],
+    "eol-last": [
+      "error",
+      "always"
+    ],
+    "comma-spacing": [
+      "error",
+      {
+        "before": false,
+        "after": true
+      }
+    ],
+    "no-trailing-whitespace": true,
+    "no-unused-expression": true,
+    "no-var-keyword": true,
+    "no-var-requires": false,
+    "no-require-imports": true,
+    "quotemark": [
+      true,
+      "double"
+    ],
+    "jsx-wrap-multiline": false,
+    "no-debugger": false,
+    "ordered-imports": [
+      false
+    ],
+    "object-literal-sort-keys": [
+      false
+    ],
+    "new-parens": true,
+    "no-arg": true,
+    "no-bitwise": true,
+    "no-conditional-assignment": true,
+    "no-consecutive-blank-lines": false,
+    "no-console": {
+      "severity": "warning",
+      "options": [
+        "debug",
+        "info",
+        "log",
+        "time",
+        "timeEnd",
+        "trace"
+      ]
+    },
+    "trailing-comma": [
+      true,
+      {
+        "singleline": "never",
+        "multiline": {
+          "objects": "never",
+          "arrays": "never",
+          "functions": "never",
+          "typeLiterals": "ignore"
+        }
+      }
+    ]
+  },
+  "jsRules": {
+    "max-line-length": {
+      "options": [
+        100
+      ]
+    }
+  },
+  "defaultSeverity": "error",
+  "linterOptions": {
+    "exclude": [
+      "node_modules/**/*.ts",
+      "dist/**/*.js",
+      "config/**/*.js"
+    ]
+  },
+  "rulesDirectory": []
+};
